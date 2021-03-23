27df3404359b73d84cb30452b9e9958df676dae9
Abdallah-Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2021-03-23 11:54:05 +0200

    Add files via upload

---
+++ Enum.java
@@ -0,0 +1,15 @@
+package pkgenum;
+
+public class Enum {
+
+    
+    public static void main(String[] args) {
+        Countries C1= Countries.Russia;
+//                Countries C2= Countries.Egypt;
+
+//        System.out.println(C2.compareTo(C1)); 
+//System.out.println(C1.ordinal());
+System.out.println(C1.toString());
+    }  
+     
+}
---
+++ Example.java
@@ -0,0 +1,14 @@
+package pkgenum;
+// Enumerated Typs 
+enum Countries{Egypt,China,Japan,Italy,Russia};
+enum  Year{First,Second,Third,Forth};
+public class Example {
+    private  String Name;
+    Countries C;
+    Year Y;
+    
+    public Example(){
+    C= Countries.China; 
+    }
+   
+}
